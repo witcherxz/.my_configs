b6ff83fb49423caf32a0d262236ac8008960a5de
witcher <witcherxzr@hotmail.coM>
2024-06-08 14:43:15 -0400

    add feature to check if repo out of sync

--- main.bash
+++ main.bash
@@ -1,1 +1,13 @@
+check_sync(){
+	git -C ~/.my_configs/ fetch origin main > /dev/null 2>&1
+	if [ ! x"$(git rev-parse main)" = x"$(git rev-parse origin/main)" ]
+	then
+    		echo 'WARN: .my_config is out of sync !!'
+	fi
+}
+
+set +m
+check_sync & disown
+set -m
+
 source ~/.my_configs/aliases.bash
